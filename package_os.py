de89b3aee25d4f72a9f4befd9d81c49a77fa2162
pjs970726 <pjs970726@gmail.com>
2021-08-14 18:22:45 +0900

    package study

---
+++ package_os.py
@@ -0,0 +1,22 @@
+import os
+from datetime import *
+
+# print(os.getcwd())
+# print(os.listdir()
+
+# try:
+#     os.mkdir('package_os')
+#     print('폴더를 만들었습니다.')
+# except FileExistsError as e:
+#     print(e)
+# try:
+#     os.rmdir('package_os')
+#     print('폴더를 삭제했습니다.')
+# except FileNotFoundError as nf:
+#     print(nf)
+#
+# print('프로그램을 종료합니다.')
+
+mod_time = os.stat('package_os').st_mtime
+print(datetime.fromtimestamp(mod_time))
+
---
+++ package_sys.py
@@ -0,0 +1,7 @@
+import sys
+# print(sys.version)
+# print(sys.copyright)
+
+# 명령 매개변수
+print(sys.argv)
+print(sys.prefix)
